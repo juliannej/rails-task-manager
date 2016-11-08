170867670c1da8c0d24b72086a32c22259d90777
Julianne Joswiak <juliannej@mac.com>
2016-11-08 12:19:05 +0000

    done but it's ugly

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,6 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :update, :edit, :destroy]
+
   def index
     @tasks = Task.all
   end
@@ -7,17 +9,34 @@ class TasksController < ApplicationController
   end
 
   def new
+    @task = Task.new
   end
 
   def create
+    @task = Task.create(task_params)
+    redirect_to task_path(@task)
   end
 
   def edit
   end
 
   def update
+    @task.update(task_params)
+    redirect_to task_path(@task)
   end
 
   def destroy
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :completed)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
   end
 end
